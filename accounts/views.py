539b7e73c9c56e80f10bda1a6642a2c031220f06
Blagoj5 <blagoj.petrov5@gmail.com>
2019-12-30 18:14:12 +0100

    fixed login/register bug, closes #11

--- accounts/views.py
+++ accounts/views.py
@@ -17,83 +17,88 @@ from django.conf import settings
 
 # Create your views here.
 def register(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name'] 
-        last_name = request.POST['last_name'] 
-        username= request.POST['username'].lower() 
-        password = request.POST['password'] 
-        password = request.POST['password'] 
-        password2 = request.POST['password2'] 
-        email = request.POST['email'] 
-        
-        # Validate passwords
-        if password == password2:
-            #Checking username
-            if User.objects.filter(username=username).exists():
-                messages.error(request, 'Username is already taken')
-                return redirect('register')
-            else:
-                if User.objects.filter(email=email).exists():
-                    messages.error(request, "There's an account already registered with this mail")
+    
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            first_name = request.POST['first_name'] 
+            last_name = request.POST['last_name'] 
+            username= request.POST['username'].lower() 
+            password = request.POST['password'] 
+            password = request.POST['password'] 
+            password2 = request.POST['password2'] 
+            email = request.POST['email'] 
+            
+            # Validate passwords
+            if password == password2:
+                #Checking username
+                if User.objects.filter(username=username).exists():
+                    messages.error(request, 'Username is already taken')
                     return redirect('register')
                 else:
-                    #Validation good
-                    user = User.objects.create_user(username=username, email=email, password=password, 
-                    first_name=first_name, last_name=last_name, is_active=False)
-                    # Get current site
-                    subject = 'Activate your Account'
-                    current_site = get_current_site(request)
-                    # create Message
-                    # message = render_to_string('accounts/account_activation_email.txt', {      #Normal string(message) in the  mail
-                    #     'user': user,
-                    #     'domain': current_site.domain,
-                    #     'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode("utf-8"),
-                    #     'token': account_activation_token.make_token(user),
-                    # })
-                    domain = current_site.domain
-                    message = render_to_string('accounts/account_activation_email.html', {
-                        'user': user,
-                        'domain': domain[:-1],
-                        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                        'token': account_activation_token.make_token(user),
-                    })
-                     # send activation link to the user
-                    user.email_user(subject=subject, message=message)
+                    if User.objects.filter(email=email).exists():
+                        messages.error(request, "There's an account already registered with this mail")
+                        return redirect('register')
+                    else:
+                        #Validation good
+                        user = User.objects.create_user(username=username, email=email, password=password, 
+                        first_name=first_name, last_name=last_name, is_active=False)
+                        # Get current site
+                        subject = 'Activate your Account'
+                        current_site = get_current_site(request)
+                        # create Message
+                        # message = render_to_string('accounts/account_activation_email.txt', {      #Normal string(message) in the  mail
+                        #     'user': user,
+                        #     'domain': current_site.domain,
+                        #     'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode("utf-8"),
+                        #     'token': account_activation_token.make_token(user),
+                        # })
+                        domain = current_site.domain
+                        message = render_to_string('accounts/account_activation_email.html', {
+                            'user': user,
+                            'domain': domain[:-1],
+                            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                            'token': account_activation_token.make_token(user),
+                        })
+                        # send activation link to the user
+                        user.email_user(subject=subject, message=message)
 
-                    messages.success(request, 'Mail has been sent')
+                        messages.success(request, 'Mail has been sent')
 
-                    context = {
-                        'current_user': user
-                    }
-                    return render(request, 'accounts/login.html', context)
+                        context = {
+                            'current_user': user
+                        }
+                        return render(request, 'accounts/login.html', context)
+            else:
+                messages.error(request, 'Passwords do not match')
+                return redirect('register')
         else:
-            messages.error(request, 'Passwords do not match')
-            return redirect('register')
-    else:
-        messages.debug(request, 'This is GET')
-        return render(request, 'accounts/register.html')
+            return render(request, 'accounts/register.html')
 
-    return render(request, 'accounts/register.html')
+    else:
+            messages.error(request, 'You already have an account')
+            return redirect('dashboard')
+
     
 def login(request):
 
-    if request.method == 'POST':
-        username = request.POST['username'].lower()
-        password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            messages.success(request, f'Welcome {username.capitalize()}, you have been successfully logged in!')
-            return redirect('dashboard')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            username = request.POST['username'].lower()
+            password = request.POST['password']
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, f'Welcome {username.capitalize()}, you have been successfully logged in!')
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Invalid login! Try again')
+                return redirect('login')        
         else:
-            messages.error(request, 'Invalid login! Try again')
-            return redirect('login')        
+            return render(request, 'accounts/login.html')
     else:
-        messages.debug(request, 'This is GET')
-        return render(request, 'accounts/login.html')
-
-    return render(request, 'accounts/login.html')
-
+        messages.error(request, 'You are already logged in!')
+        return redirect('dashboard')
+    
 
 def logout(request):
     # if request.method == 'POST':
